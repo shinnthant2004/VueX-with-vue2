949a1bff78363dd108d1b00399d6c505a0715d6b
shinthantequi@gmail.com <shinthantequi@gmail.com>
2022-04-17 08:21:27 +0630

    actions and mutations

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,8 +2,8 @@
     <div>
      <h2 class="text-primary">Hello</h2>
       <div class="container">
-          <div class="row my-4">
-              <div class="col-md-4" v-for="Todo in MyTodos" :key="Todo.id">
+          <div class="row">
+              <div class="col-md-4 my-4" v-for="Todo in MyTodos" :key="Todo.id">
                   <b-card bg-variant="primary" text-variant="white" class="text-center">
                      <b-card-text>{{ Todo.title }}</b-card-text>
                   </b-card>
@@ -15,9 +15,13 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex"
+import { mapActions, mapGetters } from "vuex"
     export default {
         computed:mapGetters(["MyTodos"]),
+        methods:mapActions(['getTodos']),
+        mounted(){
+            this.getTodos()
+        }
     }
 </script>
 
